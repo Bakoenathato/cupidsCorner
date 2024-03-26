571138ef0debedc7f57ec8d4e9b20b835112322b
Leonard Langa <221069054@mycput.ac.za>
2024-03-26 22:15:59 +0200

    IRepository

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package za.ac.cput.repo;
 
 public interface IRepository<T,ID> {
@@ -7,3 +8,19 @@ public interface IRepository<T,ID> {
     boolean delete(ID id);
 
 }
+=======
+package za.ac.cput.repository;
+
+/* IRepository.java
+   IRepository interface
+   Author: Leonard Gabriel Langa (221069054)
+   Date: 25 March 2024
+*/
+
+public interface IRepository <T, ID>
+{
+    public T create(T t);
+    public T read(ID id);
+    public T update(T t);
+    public boolean delete(ID id);}
+>>>>>>> 1c3c743 (IRepository)
