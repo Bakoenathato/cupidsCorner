e2b3c2c4c1a8b9e1080c719908ac3fa9a3660301
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-03-26 19:07:50 +0200

    Add files via upload

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,4 +1,16 @@
 package za.ac.cput.util;
 
+import java.util.UUID;
+
 public class Helper {
+    public static boolean isNullorEmpty(String s){
+        if(s==null||s.isEmpty())
+            return true;
+        return false;
+    }
+
+
+    public static String generateID(){
+        return UUID.randomUUID().toString();
+    }
 }
