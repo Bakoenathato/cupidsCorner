aff68fc4ba35e46307214cd4a3394b537e3008d7
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-03-27 23:38:56 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/factory/UserprofileFactoryTest.java
@@ -0,0 +1,34 @@
+package za.ac.cput.factory;
+//UserprofileFactoryTest.java
+//UserProfile Test class
+//Author:Braedon Sidney Mullins(222821825)
+//Date:27 March 2024
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.UserProfile;
+import za.ac.cput.util.Helper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserprofileFactoryTest {
+    private UserProfile e=new UserProfile();
+    //private Helper x=new Helper();
+
+    @Test
+    void createUser() {
+        UserProfile user= UserprofileFactory.createUser("Braedon","Mullins","Male", 21 ,"192037977239","Kuilsriver","Likes cars");
+        Boolean check=Helper.isNullOrEmpty(String.valueOf(user));
+        assertNotNull(check);
+        System.out.println(user);
+
+    }
+
+    @Test
+    void testCreateUser() {
+        String ID=Helper.generateID();
+        UserProfile user= UserprofileFactory.createUser("Braedon","mullins","Male",21,ID,"KuilsRiver","Like Cars");
+        Boolean check=Helper.isNullOrEmpty(String.valueOf(user));
+        assertNotNull(check);
+        System.out.println(user);
+    }
+}
