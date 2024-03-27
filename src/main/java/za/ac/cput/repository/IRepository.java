c7fdc2a96a8530bcebf861771366444ae4d53672
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-03-27 20:31:14 +0200

    Add files via upload

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.repo;
+//IRepository.java
+//UserProfile Repo class
+//Author:Braedon Sidney Mullins(222821825)
+//Date:27 March 2024
 
 public interface IRepository<T,ID> {
     T create(T t);
--- src/main/java/za/ac/cput/repository/UserprofileRepository.java
+++ src/main/java/za/ac/cput/repository/UserprofileRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.repo;
+//UserprofileRepository.java
+//UserProfile Repo class
+//Author:Braedon Sidney Mullins(222821825)
+//Date:27 March 2024
 
 import za.ac.cput.domain.UserProfile;
 
