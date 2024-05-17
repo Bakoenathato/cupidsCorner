3ab9ddb08983e77bc1eea722a6e5786ab5e4ac49
thato <222667087@mycput.a.c.za>
2024-05-17 23:56:35 +0200

    match and Preference

---
+++ src/test/java/za/ac/cput/factory/PreferenceFactoryTest.java
@@ -0,0 +1,35 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.Preference;
+import za.ac.cput.domain.UserProfile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    MatchingFactoryTest.java
+    MatchingFactory class
+    Author: Thato Mokoena (222667087)
+    Date 19 May 2024
+*/
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class PreferenceFactoryTest {
+
+//    private Preference preference;
+//
+//    @Test
+//    void a_buildPreference() {
+//        preference = PreferenceFactory.buildPreference(1, "21-32", "-20KM", "Female");
+//        assertNotNull(preference);
+//        System.out.println(preference);
+//    }
+//
+//    @Test
+//    void b_buildPreferenceWithFail() {
+//        preference = PreferenceFactory.buildPreference(1,"21-32", "-20KM", "Female");
+//
+//    }
+}
