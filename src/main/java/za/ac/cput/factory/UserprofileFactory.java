2f04401fab327d94339d7cbcacd410a4294b58a5
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-05-17 20:42:11 +0200

    Add files via upload

--- src/main/java/za/ac/cput/factory/UserprofileFactory.java
+++ src/main/java/za/ac/cput/factory/UserprofileFactory.java
@@ -1,23 +1,39 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Location;
+import za.ac.cput.domain.User;
 import za.ac.cput.domain.UserProfile;
 import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 //UserprofileFactory.java
 //UserProfile Factory class
 //Author:Braedon Sidney Mullins(222821825)
 //Date:27 March 2024
 
 public class UserprofileFactory {
-    public static UserProfile createUser(String Name,String Surname,String Gender,double Age,String ID,String Location,String Bio){
-        if(Helper.isNullOrEmpty(Name)||Helper.isNullOrEmpty(Surname)||Helper.isNullOrEmpty(ID)||Helper.isNullOrEmpty(String.valueOf(Age))||Helper.isNullOrEmpty(Location)||Helper.isNullOrEmpty(Bio)||Helper.isNullOrEmpty(Gender))
+    private User user;
+    private Location location;
+    public static UserProfile createUser(String profileID, User userID, String intrest, String profile_vis, LocalDateTime created_at, Location LocationID){
+        if(Helper.isNullOrEmpty(profileID)||Helper.isNullOrEmpty(String.valueOf(userID))||Helper.isNullOrEmpty(intrest)||Helper.isNullOrEmpty(String.valueOf(profile_vis))||Helper.isNullOrEmpty(String.valueOf(created_at))||Helper.isNullOrEmpty(String.valueOf(LocationID)))
             return null;
-        return new UserProfile.Builder().setName(Name).setSurname(Surname).setID(ID).setAge(Age).setLocation(Location).setBio(Bio).setGender(Gender).build();
+        return new UserProfile.Builder().setProfileID(profileID).setUserID(userID).setIntrests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(LocationID).build();
     }
-    public static UserProfile createUser(String Name,String Surname,String Gender,double Age,String Location,String Bio){
-        if (Helper.isNullOrEmpty(Name)||Helper.isNullOrEmpty(Surname)||Helper.isNullOrEmpty(Gender)||Helper.isNullOrEmpty(String.valueOf(Age))||Helper.isNullOrEmpty(Location)||Helper.isNullOrEmpty(Bio))
-            return null;
 
-        String ID=Helper.generateId();
-        return new UserProfile.Builder().setName(Name).setSurname(Surname).setID(ID).setAge(Age).setLocation(Location).setBio(Bio).build();
+
+
+
+    public static UserProfile createUser(String intrest, String profile_vis, LocalDateTime created_at){
+        if (Helper.isNullOrEmpty(intrest)||Helper.isNullOrEmpty(String.valueOf(profile_vis))||Helper.isNullOrEmpty(String.valueOf(created_at)))
+        return null;
+
+        String profileID=Helper.generateId();
+
+        User user=UserFactory.buildUser("braedonmullins01@gmail.com","Password");
+        Location location1=LocationFactory.createLocation("Cape Town","Western Cape","Kuilsriver");
+
+//        User userid=user.getUserId();
+        return new UserProfile.Builder().setProfileID(profileID).setUserID(user).setIntrests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(location1).build();
     }
 }
