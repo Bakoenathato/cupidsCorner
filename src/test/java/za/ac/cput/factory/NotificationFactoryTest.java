822dd6a7ff8b7e2003d6d947a0ff399d111bff1b
Leonard Langa <127895655+LeonardLanga@users.noreply.github.com>
2024-05-17 21:44:44 +0000

    Notification and Report

--- src/test/java/za/ac/cput/factory/NotificationFactoryTest.java
+++ src/test/java/za/ac/cput/factory/NotificationFactoryTest.java
@@ -13,37 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NotificationFactoryTest
 {
+    
 
-
-    @Test
-    void firstTestBuildNotification()
-    {
-        Notification notification = NotificationFactory.buildNotification("300","You have a message from User200 **Message : Hello, how are you doing**", "Message","201","200","100");
-        assertNotNull(notification);
-        System.out.println(notification);
-    }
-
-    @Test
-    void firstTestBuildNotificationWithFail()
-    {
-        Notification notification = NotificationFactory.buildNotification("","You have been matched with User202","match","203","202","500");
-        assertNotNull(notification);
-        System.out.println(notification);
-    }
-
-    @Test
-    void secondTestBuildNotification()
-    {
-        Notification notification = NotificationFactory.buildNotification("You have been matched with User204", "match","205","204","500");
-        assertNotNull(notification);
-        System.out.println(notification);
-    }
-
-    @Test
-    void secondTestBuildNotificationWithFail()
-    {
-        Notification notification = NotificationFactory.buildNotification("You have been matched with User---", "match","207","","500");
-        assertNotNull(notification);
-        System.out.println(notification);
-    }
 }
---
+++ src/test/java/za/ac/cput/factory/ReportFactoryTest.java
@@ -0,0 +1,16 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportFactoryTest {
+
+    @Test
+    void buildReport() {
+    }
+
+    @Test
+    void testBuildReport() {
+    }
+}
