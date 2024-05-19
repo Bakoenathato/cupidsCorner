f26d35e99322cf2ca6a6770770578e7b6f59aa79
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-05-19 12:58:21 +0200

    Add files via upload

--- src/main/java/za/ac/cput/factory/UserprofileFactory.java
+++ src/main/java/za/ac/cput/factory/UserprofileFactory.java
@@ -1,9 +1,6 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.Like;
-import za.ac.cput.domain.Location;
-import za.ac.cput.domain.User;
-import za.ac.cput.domain.UserProfile;
+import za.ac.cput.domain.*;
 import za.ac.cput.util.Helper;
 
 import java.time.LocalDate;
@@ -18,13 +15,13 @@ import java.util.prefs.PreferencesFactory;
 public class UserprofileFactory {
     private User user;
     private Location location;
-    private Prefernce prefernce;
+    private Preference prefernce;
     private Like like;
     private LocalDateTime time;
-    public static UserProfile createUser(String profileID, User userID, String intrest, boolean profile_vis, LocalDateTime created_at, Location LocationID, Preference prefernceID, Like like){
-        if(Helper.isNullOrEmpty(profileID)||Helper.isNullOrEmpty(String.valueOf(userID))||Helper.isNullOrEmpty(intrest)||Helper.isNullOrEmpty(String.valueOf(profile_vis))||Helper.isNullOrEmpty(String.valueOf(created_at))||Helper.isNullOrEmpty(String.valueOf(LocationID))||Helper.isNullOrEmpty(String.valueOf(prefernceID))|Helper.isNullOrEmpty(String.valueOf(like)))
+    public static UserProfile createUser(String profileID, User userID, String intrest, boolean profile_vis, LocalDateTime created_at, Location LocationID, Preference prefernceID, Like like,Match matchrequest,Match matchedaccpeted){
+        if(Helper.isNullOrEmpty(profileID)||Helper.isNullOrEmpty(String.valueOf(userID))||Helper.isNullOrEmpty(intrest)||Helper.isNullOrEmpty(String.valueOf(profile_vis))||Helper.isNullOrEmpty(String.valueOf(created_at))||Helper.isNullOrEmpty(String.valueOf(LocationID))||Helper.isNullOrEmpty(String.valueOf(prefernceID))|Helper.isNullOrEmpty(String.valueOf(like))||Helper.isNullOrEmpty(String.valueOf(matchrequest))||Helper.isNullOrEmpty(String.valueOf(matchedaccpeted)))
             return null;
-        return new UserProfile.Builder().setProfileID(profileID).setUserID(userID).setIntrests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(LocationID).setPrefernceID(prefernceID).setLikes(like).build();
+        return new UserProfile.Builder().setProfileID(profileID).setUserID(userID).setInterests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(LocationID).setPreferenceID(prefernceID).setLikes(like).build();
     }
 
 
@@ -36,12 +33,18 @@ public class UserprofileFactory {
 
         String profileID=Helper.generateId();
 
-        User user=UserFactory.buildUser("braedonmullins01@gmail.com","Password");
+        User user=UserFactory.buildUser("1","braedon","braedonmullins01@gmail.com","Bredon","Mullins","male","braedon");
         Location location1=LocationFactory.createLocation("Cape Town","Western Cape","Kuilsriver");
-       // Preference prefernce= PreferenceFactory.createPreference();
+        Preference prefernce= PreferenceFactory.buildPreference(1,"20","50km","female");
         Like like=LikeFactory.buildLike("1","01","001",LocalDateTime.now());
+//        Match match=MatchFactory.buildMatch()
 
 //        User userid=user.getUserId();
-        return new UserProfile.Builder().setProfileID(profileID).setUserID(user).setIntrests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(location1).setPrefernceID().setLikes(like).build();
+        return new UserProfile.Builder().setProfileID(profileID).setUserID(user).setInterests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(location1).setPreferenceID(prefernce).setLikes(like).build();
+    }
+    public static UserProfile createUser(String profileID, User userID, String intrest, boolean profile_vis, LocalDateTime created_at, Location LocationID, Preference prefernceID, Like like){
+        if(Helper.isNullOrEmpty(profileID)||Helper.isNullOrEmpty(String.valueOf(userID))||Helper.isNullOrEmpty(intrest)||Helper.isNullOrEmpty(String.valueOf(profile_vis))||Helper.isNullOrEmpty(String.valueOf(created_at))||Helper.isNullOrEmpty(String.valueOf(LocationID))||Helper.isNullOrEmpty(String.valueOf(prefernceID))|Helper.isNullOrEmpty(String.valueOf(like)))
+            return null;
+        return new UserProfile.Builder().setProfileID(profileID).setUserID(userID).setInterests(intrest).setProfile_visibility(profile_vis).setCreated_at(created_at).setLocationID(LocationID).setPreferenceID(prefernceID).setLikes(like).build();
     }
 }
