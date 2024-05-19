32ec5b7b205f1a0130334dd7bd99df81e07a37db
Uzziah <222135654@mycput.ac.za>
2024-05-19 21:06:06 +0200

    Service stuff

---
+++ src/main/java/za/ac/cput/service/LocationService.java
@@ -0,0 +1,47 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Location;
+import za.ac.cput.repository.LocationRepository;
+
+import java.util.List;
+
+@Service
+public class LocationService implements ILocationService {
+
+    private final LocationRepository repository;
+
+    @Autowired
+    public LocationService(LocationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Location create(Location location) {
+        return repository.save(location);
+    }
+
+    @Override
+    public Location read(Long postalCode) {
+        return repository.findById(postalCode).orElse(null);
+    }
+
+    @Override
+    public Location update(Location location) {
+        if (repository.existsById(location.getPostalCode())) {
+            return repository.save(location);
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Long postalCode) {
+        repository.deleteById(postalCode);
+    }
+
+    @Override
+    public List<Location> getAll() {
+        return repository.findAll();
+    }
+}
